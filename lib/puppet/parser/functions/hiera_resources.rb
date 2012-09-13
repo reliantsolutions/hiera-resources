aab34ebdae5b2be70c0c5b6bd10f999eed956144
windowsrefund <me@angel>
2012-09-13 17:27:13 -0400

    latest function

--- lib/puppet/parser/functions/hiera_resources.rb
+++ lib/puppet/parser/functions/hiera_resources.rb
@@ -1,24 +1,22 @@
-# Usage examples:
-#   hiera_resources('key')
-# a default can be specified which is used when the key is not found
-#   hiera_resources('missing_key', $default)
+# hiera_resources - A Hiera wrapper for Puppet's create_resources function
+#
 Puppet::Parser::Functions.newfunction(:hiera_resources) do |args|
 
-  raise Puppet::Error, "hiera_resources requires 2 arguments; got %s" % args.length unless args.length >= 2
-
-  if args[2]
-    raise Puppet::Error, "hiera_resources expects a hash as the 3rd argument; got %s" % args[2].class unless args[2].is_a? Hash
+  def error(message)
+    raise Puppet::Error, message
   end
 
-  res_type = args[0]
+  file_name = File.basename(__FILE__, File.extname(__FILE__))
 
-  # The default is optional in order to permit Heira to fail when the following is true:
-  # - key is not found
-  # - no default provided
-  res_attr = args[1, 2]
+  error("%s requires 1 argument" % file_name) unless args.length >= 1
 
-  # notice how we avoid passing a static default to the hiera_hash function...
-  answer = function_hiera_hash(res_attr)
-  method = Puppet::Parser::Functions.function :create_resources
-  send(method, [res_type, answer]) unless answer.empty?
+  if args[1]
+    error("%s expects a hash as the 2nd argument; got %s" % [file_name, args[1].class]) unless args[1].is_a? Hash
+  end
+
+  function_hiera_hash(args).each do |type, resources|
+    # function_create_resources is no workie so we'll do this
+    method = Puppet::Parser::Functions.function :create_resources
+    send(method, [type, resources])
+  end
 end
